e29dbde209fa8fd6289371c56831ff3c6796770b
Neo <koromiko1104@gmail.com>
2017-06-30 01:27:43 +0800

    reduce image async flashing

--- Avengers/util/UIImageViewHelper.swift
+++ Avengers/util/UIImageViewHelper.swift
@@ -9,20 +9,48 @@
 import Foundation
 import UIKit
 
+/// Remote Image donwload helper
 extension UIImageView {
     
     func loadImage( with url: URL ) {
-        self.image = nil
-        ApiManager.downloadData(from: url, complete: { (success, imageData, error) in
+        
+        // Reduce the flash (1)
+        if self.requestingURL != url.absoluteString {
+            self.image = nil
+        }
+        
+        /// In case that the unvisible cell callbacks interupt the visible ones, check requesting urls before putting on the downloaded images (2)
+        self.requestingURL = url.absoluteString
+        
+        ApiManager.downloadData(from: url, complete: { [weak self] (success, imageData, error) in
             if success {
-                if let imageData = imageData {
+                if let imageData = imageData, let strongSelf = self {
                     DispatchQueue.main.async {
-                        let image = UIImage(data: imageData )
-                        self.image = image
+                        if strongSelf.requestingURL == url.absoluteString {
+                            let image = UIImage(data: imageData )
+                            strongSelf.image = image
+                        }
                     }
                 }
             }
         })
+    }
+    
+    
+}
+
+/// extension property implementation
+extension UIImageView {
+    
+    private struct AssociatedKeys {
+        static var requestingURL: UInt8 = 0
+    }
+    
+    var requestingURL: String {
+        get {
+            return (objc_getAssociatedObject(self, &AssociatedKeys.requestingURL ) as? String) ?? ""
+        }
+        set { objc_setAssociatedObject(self, &AssociatedKeys.requestingURL, newValue, .OBJC_ASSOCIATION_RETAIN_NONATOMIC) }
     }
 
 }
